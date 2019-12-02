93b549162755c0f54a5b66caa6e864035f8dce46
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-02 11:01:20 -0600

    finished first attempt at exercise

---
+++ js/external.js
@@ -0,0 +1,31 @@
+// first portion of the exercise
+// alert("Welcome to my Website!")
+//
+// var inputColor = prompt("What is your favorite color?")
+// console.log(inputColor);
+//
+// alert("That's my favorite color too!")
+
+// second portion of the exercise
+
+// part one
+// var littleMermaid = prompt("How many days will you be renting The Little Mermaid?")
+// alert("Your rental will cost $")
+// console.log(littleMermaid);
+// var hercules = prompt("How many days will you be renting Hercules?")
+// alert("Your rental will cost $")
+// console.log(hercules);
+// var brotherBear = prompt("How many days will you be renting Brother Bear?")
+// alert("Your rental will cost $")
+// console.log(brotherBear);
+
+// part two
+var googlePay = prompt("How many hours have you worked for Google this week?")
+alert("You made $")
+console.log(googlePay);
+var amazonPay = prompt("How many hours have you worked for Amazon this week?")
+alert("You made $")
+console.log(amazonPay);
+var facebookPay = prompt("How many hours have you worked for Facebook this week?")
+alert("You made $")
+console.log(facebookPay);
