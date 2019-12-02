61814007e4b456e6ae93977666f5fce8cd06a694
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-02 14:21:04 -0600

    completed exercise to working versions

--- js/external.js
+++ js/external.js
@@ -1,64 +1,53 @@
+"use strict";
+
 // first portion of the exercise
-//  alert("Welcome to my Website!")
-//
-//  var inputColor = prompt("What is your favorite color?")
-//
-//  console.log(inputColor);
-//
-//  alert("That's my favorite color too!")
+//   alert("Welcome to my Website!")
+// var inputColor = prompt("What is your favorite color?")
+// var alertMessage = "Great, " + inputColor + " is my favorite color too.";
+//   alert(alertMessage);
+
+
 // second portion of the exercise
 
 // part one
-// var littleMermaid = prompt("How many days will you be renting The Little Mermaid?")
-// var hercules = prompt("How many days will you be renting Hercules?")
-// var brotherBear = prompt("How many days will you be renting Brother Bear?")
-// var movies = ((Number(littleMermaid) + Number(hercules) + Number(brotherBear)) * 3)
-// alert("Your rental will cost $" + (movies))
 
+// var dailyRentalRate = Number(prompt("How much is the daily rental rate?"))
+
+// var littleMermaid = Number(prompt("How many days will you be renting The Little Mermaid?"))
+// var hercules = Number(prompt("How many days will you be renting Hercules?"))
+// var brotherBear = Number(prompt("How many days will you be renting Brother Bear?"))
+
+// var moviesTotal = (((littleMermaid) + (hercules) + (brotherBear)) * (dailyRentalRate))
+// alert("Your rental will cost $" + (moviesTotal))
+
+// // // part two
+
+// var googleRate = Number(prompt("What is your Google rate?"))
+// var amazonRate = Number(prompt("What is your Amazon rate?"))
+// var facebookRate= Number(prompt("What is your Facebook rate?"))
 //
-// // part two
-// var googlePay = prompt("How many hours have you worked for Google this week?")
-// var amazonPay = prompt("How many hours have you worked for Amazon this week?")
-// var facebookPay = prompt("How many hours have you worked for Facebook this week?")
-// var weekPay = ((Number(googlePay)*400 + Number(amazonPay)*380 + Number(facebookPay)*350))
+// var googlePay = Number(prompt("How many hours have you worked for Google this week?"))
+// var amazonPay = Number(prompt("How many hours have you worked for Amazon this week?"))
+// var facebookPay = Number(prompt("How many hours have you worked for Facebook this week?"))
+//
+// var weekPay = (googlePay * googleRate) + (amazonPay * amazonRate) + (facebookPay * facebookRate)
 // alert("You made $" + (weekPay))
 
-
 // part three
-var classSize = prompt("How many students are in this class?")
-var classTime = prompt("Does this class time conflict with your schedule?")
-var classTime = function(classTime) {
-    if (classTime > 10 ){
-        return  "You may schedule this class";
-    } else {
-    return "You cannot schedule this class time."
-    }
-}
-console.log(morningClass(11));
-console.log(morningClass(8));
 
-var classSize = function(numStudents) {
-    if (numStudents < 130) {
-        return "This class has open seats."
-    } else {
-        return "This class is full."
-    }
-}
-console.log(classSize(180));
-console.log(classSize(90));
+// var classIsNotFull = confirm("Class is not full");
+// var noScheduleConflicts = confirm("Schedules do not conflict");
+// console.log("class is not full: " + classIsNotFull);
+// console.log("there are no schedule conflicts: " + noScheduleConflicts);
+//
+// var studentEnrolled = classIsNotFull && noScheduleConflicts;
+// console.log(studentEnrolled);
 
 // part four
-// var offerApplied = function(numItems) {
-//     if (numItems >= 2) {
-//         return "Offer Applied!"
-//     }
-// }
-// console.log(offerApplied(3));
-// var offerExpired = function (dateInput) {
-//     if (dateInput < 12/2/2019) {
-//         return "Sorry, this offer has expired."
-//     } else {
-//         return "Offer Applied!"
-//     }
-// }
-// console.log(offerExpired(12/1/2019));
+//
+// var numberOfItems = Number(prompt("How many items for checkout?"))
+// var offerValid = confirm("Is the offer still valid?")
+// var isPremiumMember = confirm("Are you a premium member?")
+//
+// var discountApplied = offerValid && (isPremiumMember || numberOfItems > 2);
+// alert("Can use discount: " + discountApplied)
