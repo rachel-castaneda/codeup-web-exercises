3b959b0393ef6a88c55722fa6a95f1428ebca697
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-03 11:03:03 -0600

    worked on edabit exercises

---
+++ js/edabit-challenges.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// 1. function called hello that return the string "hello, edabit.com"
+function hello() {
+    return "hello edabit.com"
+}
+
+// 2. function called getFirstValue that returns the first item of an array
+function getFirstValue(arr) {
+    return arr[0]
+}
+
+// 3. function called howManySeconds that takes in a number of hours and converts it to seconds
+function howManySeconds(hours) {
+    return (hours * 3600)
+}
+
+// 4. function called triArea that takes in the base and height of triangle and returns the area of the triangle
+function triArea(base, height) {
+    return (base * height)/2
+}
+
+// 5. function called nextEdge that takes in the 2 sides and returns the maximum range for the third edge
+function nextEdge(side1, side2) {
+    return (side1 + side2)-1
+}
+
+// 6. function that returns true if the values are equal and false if they are not equal values
+function checkEquality(a, b) {
+    return a === b
+}
+
+// 7. function called remainder takes in 2 numbers and returns the remainder of those numbers
+function remainder(x, y) {
+    return x % y
+}
+
+// 8. function that takes in the number of each animal and returns the total number of legs for all animals
+function animals(chickens, cows, pigs) {
+    return (chickens*2) + (cows*4) + (pigs*4)
+}
+
+// 9.
+
+
+// 10.
+
+
+// 11.
+
+
+// 12.
+
+
+// 13.
+
+
+// 14.
+
+
+// 15.
