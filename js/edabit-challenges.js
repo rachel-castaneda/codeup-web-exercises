5bd4f070a1daf080062eb7340a5ae4d827944258
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-04 13:41:27 -0600

    worked on edabit challenges

--- js/edabit-challenges.js
+++ js/edabit-challenges.js
@@ -40,22 +40,99 @@ function animals(chickens, cows, pigs) {
     return (chickens*2) + (cows*4) + (pigs*4)
 }
 
-// 9.
+// 9. function that takes a number and returns true if number is divisible by 5
+function divisibleByFive(n) {
+    return n %5 === 0
+}
+
+// 10. function that returns true if the product of "prob" and "prize" is greater than the value of "pay"
+function profitableGamble(prob, prize, pay) {
+    if ((prob * prize)> pay){
+        return true
+    } else {
+        return false
+    }
+}
+
+// 11. a function that takes in a number and returns the value of that number squared
+function squared(b) {
+    return b*b;
+}
+
+// 12. function that takes in a string and returns the length
+function length(str) {
+    return str.length
+}
+
+// 13. function that takes in a number and returns true if the value of that number is less than or equal to 0, else returns false
+function lessThanOrEqualToZero(num) {
+    if (num <= 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// 14. function that takes in an array and returns the array in reverse
+function reverse(arr) {
+    return arr.reverse()
+}
+
+// 15. function that takes in a number and returns true if that number is divisible by 100
+function divisible(num) {
+    if (num % 100 ===0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// 16. function that takes in an array and returns the last item of that array
+function getLastItem(arr) {
+    return arr.pop()
+}
+
+// 17. function that takes argument object "sizes" and returns the value of the volume of a box (height, width, length)
+function volumeOfBox(sizes) {
+    return sizes.height * sizes.width * sizes.length;
+}
+
+// 18. using arrow function instead of normal function
+// const helloName = name => "Hello "+ name + "!"
+// helloName("Ed") ➞ "Hello Ed!"
+
+// 19. function that returns true if the length of string1 is equal to the length of string2
+function comp(str1, str2) {
+    if (str1.length === str2.length) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// 20. function that takes in a number and returns the number incremented by 1
+function addition(num) {
+    return num +1
+}
+
+// 21. function that takes a number and returns "even" if that number is even, else returns "odd"
+function isEvenOrOdd(num) {
+    if (num % 2 ==0) {
+        return "even"
+    } else {
+        return "odd"
+    }
+}
+
+// 22. function that takes in hours and minutes, converts them to seconds and returns the sum
+function convert(hours, minutes) {
+    return (hours * 3600) + (minutes *60)
+}
+
+// 23.
 
 
-// 10.
+// 24.
 
 
-// 11.
-
-
-// 12.
-
-
-// 13.
-
-
-// 14.
-
-
-// 15.
+// 25.
