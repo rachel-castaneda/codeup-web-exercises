62e925eb6d5c5b07df7cbbd1795f5d41eddb28bf
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-05 16:24:59 -0600

    worked on edabit

--- js/edabit-challenges.js
+++ js/edabit-challenges.js
@@ -129,10 +129,113 @@ function convert(hours, minutes) {
     return (hours * 3600) + (minutes *60)
 }
 
-// 23.
+// 23. function that takes an array and returns the index of an item
+function search(arr, item) {
+    return arr.indexOf(item)
+}
+
+// 24. function that takes in an array and a string and returns the index of the string
+function find_index(arr, str) {
+    return arr.indexOf(str)
+}
+
+// 25. function that takes in two arrays and returns the arrays concatenated
+function concat(arr1, arr2) {
+    return arr1.concat(arr2)
+}
+
+// 26. function that takes a string and returns true if that string is empty and false otherwise
+function isEmpty(s) {
+    return !s
+}
+
+// 27. function that takes in two strings and returns them concatenated
+function concatName(firstName, lastName) {
+    return lastName + ", " + firstName;
+}
+
+// 28.
+function scoreCalculator(easy, med, hard) {
+    //easy 5, med 10, hard 20
+    if (easy >= 0 && med >= 0 && hard >= 0) {
+        return (easy * 5) + (med * 10) + (hard * 20);
+    } else {
+        return "invalid";
+    }
+}
+// console.log(scoreCalculator(2,-5,5))
+
+// 29. function that takes in two numbers and returns the sum
+function addition(a, b) {
+    return a + b
+}
+
+// 30. function that returns the number of arguments it is called with
+function numArgs() {
+    return arguments.length
+}
+
+// 31. function that takes integer minutes and returns the value in seconds
+function convert(minutes) {
+    return parseInt(minutes * 60)
+}
+
+// 32. function that returns true if the string includes spaces
+function hasSpaces(str) {
+    return str.includes(' ')
+}
+
+// 33. function that checks if an array includes a specific value
+function check(arr, el) {
+    return arr.includes(el)
+}
+
+// 34.
 
 
-// 24.
+// 35.
 
 
-// 25.
+// 36.
+
+
+// 37.
+
+
+// 38.
+
+
+// 39.
+
+
+// 40.
+
+
+// 41.
+
+
+// 42.
+
+
+// 43.
+
+
+// 44.
+
+
+// 45.
+
+
+// 46.
+
+
+// 47.
+
+
+// 48.
+
+
+// 49.
+
+
+// 50.
