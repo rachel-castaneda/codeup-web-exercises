0207a3833d16d9ea8215d79414832ea6be057d11
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-09 11:57:34 -0600

    finished multiplication table exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,3 +1,24 @@
+"use strict";
+
+// 7x1 =7
+// 7x2 =14
+// 7x3 =21
+// 7x4 =28
+// 7x5 =35
+// 7x6 =42
+// 7x8 =56
+// 7x9 =63
+// 7x10 =70
+
+// function showMulpitiplicationTable(numberInput) {
+//     for(var i = 1; i <= 10; i++){
+//         var answer = numberInput * i;
+//         console.log(numberInput + " x " + i + " = " + answer);
+//     }
+// }
+// showMulpitiplicationTable(7);
+
+
 // for (var i = 20; i <=200; i++){
 //     if(i %2===0) {
 //         console.log(i + " is even");
