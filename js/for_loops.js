3d19192cb04e963504240c7a6e167c4c3fb124fc
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-09 12:04:21 -0600

    completed random number between 20-200 exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -19,13 +19,12 @@
 // showMulpitiplicationTable(7);
 
 
-// for (var i = 20; i <=200; i++){
-//     if(i %2===0) {
-//         console.log(i + " is even");
-//     } else if (i %2!==0){
-//         console.log(i + " is odd")
+// for (var i = 1; i <=10; i++){
+//     var randomNumber = Math.floor(Math.random() * 20) + 180;
+//     if(randomNumber %2===0) {
+//         console.log(randomNumber + " is even");
 //     } else {
-//         console.log(i)
+//         console.log(randomNumber + " is odd")
 //     }
 // }
 
