19bc8092d09388cebd2cfa000d4f540b83d20230
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-10 10:08:49 -0600

    worked on manipulating arrays

--- js/arrays-lec.js
+++ js/arrays-lec.js
@@ -1,15 +1,49 @@
-var pets = [];
- pets[0] = "toby";
- pets[1] = "princess";
- pets[2] = "samson";
- pets[3] = "jill";
- pets[4] = "bubbles";
- pets[5] = "malu";
+"use strict";
 
- for (var i = 0; i < pets.length; i++) {
-     console.log(pets[i]);
- }
+// intro to arrays, iterating arrays
 
-pets.forEach(function (pet, i, arr) {
-    console.log(pet)
-})
+// var pets = [];
+//  pets[0] = "toby";
+//  pets[1] = "princess";
+//  pets[2] = "samson";
+//  pets[3] = "jill";
+//  pets[4] = "bubbles";
+//  pets[5] = "malu";
+//
+//  for (var i = 0; i < pets.length; i++) {
+//      console.log(pets[i]);
+//  }
+//
+// pets.forEach(function (pet, i, arr) {
+//     console.log(pet)
+// })
+
+
+// manipulating arrays //
+
+// var fruit = ['apple', 'orange', 'banana'];
+ // fruit[fruit.length] = 'pear'    // add item to end of array, this method not really used; as opposed to the 'push' method
+// console.log(fruit);
+
+// fruit.push('pear');  // adds item to end of array
+// fruit.push('pineapple');  // adds item to end of array
+// fruit.push('grape');  // adds item to end of array
+// fruit.push('mango');  // adds item to end of array
+
+// console.log(fruit.shift())  // fruit.shift() removes item from the start of the array
+// fruit.unshift('strawberry') // adds item to the start of the array
+
+// console.log(fruit.pop())    // pulls item off the end of an array, opposite to the 'push' method
+
+// console.log(fruit); // final array after manipulation
+
+// var dogs = ['toby', 'gus', 'bullet', 'duncan'];
+// console.log(dogs);
+// dogs.push('lucky')
+// console.log(dogs)
+// dogs.pop()
+// console.log(dogs)
+// dogs.shift()
+// console.log(dogs)
+// dogs.unshift('lucky')
+// console.log(dogs)
