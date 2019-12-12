3d9f619243a68898c6fa5c4589cc0fe545d15176
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-12 11:17:47 -0600

    finished math exercise

--- js/circle.js
+++ js/circle.js
@@ -5,32 +5,37 @@
     var circle = {
         radius: 3,
 
-        getArea: function () {
+        getArea: function (radius) {
+            var area = Math.PI*(Math.pow(this.radius, 2))
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return; // TODO: return the proper value
+            return area; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
+            if (doRounding) {
+                // var area = this.getArea();
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea()));
+            } else {
+                // If doRounding is true, round the result to the nearest integer.
+                // Otherwise, output the complete value
 
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+            }
         }
     };
 
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
+    console.log("Circle information rounded to the nearest whole number" );
     circle.logInfo(true);
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
