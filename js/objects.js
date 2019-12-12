7e5130ad7007d08749d09f00d24238f1e3d43f2d
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-11 21:07:31 -0600

    file changes

--- js/objects.js
+++ js/objects.js
@@ -145,6 +145,10 @@
      *      ...
      */
 
+    for (var i= 0; i< 20; i++) {
+        console.log(Math.floor(Math.random()*100))
+    }
+
     /**
      * Bonus:
      * - Create a function named `createBook` that accepts a title and author
