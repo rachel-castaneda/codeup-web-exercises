5d7ee6f939d68bcb23f7da5b007f9d8eb23907a6
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-01-24 10:11:21 -0600

    worked on promises

--- js/promises.js
+++ js/promises.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const wait = (num) => {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            if (num > 2000 ) {
+                resolve();
+            } if (num %2 === 0) {
+                resolve();
+            } else {
+                reject()
+            }
+        }, num);
+
+    });
+}
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+wait(5000).then(() => console.log('You\'ll see this after 5 seconds'));
+
